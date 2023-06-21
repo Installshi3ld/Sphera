f02fa440bfc3f17fdb9943604ab1f9ae802aa6ba
Installshi3ld <61904137+Installshi3ld@users.noreply.github.com>
2023-06-21 14:43:01 +0200

    Selection system on Difficulty select screen + few modification

--- Source/VineMobile/VineMobile.Build.cs
+++ Source/VineMobile/VineMobile.Build.cs
@@ -8,7 +8,7 @@ public class VineMobile : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore",});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
